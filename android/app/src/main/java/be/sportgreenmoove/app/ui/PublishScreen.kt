eb52ce229afc1d8b6064683f2ba728ee0e279bda
mrr <mohamed-redha@hotmail.fr>
2026-06-11 14:13:09 +0100

    Fix Android publish screen clickable import

--- android/app/src/main/java/be/sportgreenmoove/app/ui/PublishScreen.kt
+++ android/app/src/main/java/be/sportgreenmoove/app/ui/PublishScreen.kt
@@ -3,6 +3,7 @@ package be.sportgreenmoove.app.ui
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
