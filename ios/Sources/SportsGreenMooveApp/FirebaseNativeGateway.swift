d7bb4144b0a834dec5502f3a6178ec89cd1c4666
mrr <mohamed-redha@hotmail.fr>
2026-06-12 11:44:09 +0100

    Fix iOS Firebase Functions concurrency import

--- ios/Sources/SportsGreenMooveApp/FirebaseNativeGateway.swift
+++ ios/Sources/SportsGreenMooveApp/FirebaseNativeGateway.swift
@@ -4,7 +4,7 @@ import Foundation
 import FirebaseAuth
 import FirebaseCore
 import FirebaseFirestore
-import FirebaseFunctions
+@preconcurrency import FirebaseFunctions
 
 @MainActor
 enum AppRuntime {
